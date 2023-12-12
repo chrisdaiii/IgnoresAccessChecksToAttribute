7970adb70d5c927dbe5f0f5149844e94500d5696
Chris Dai <67849861+chrisdaiii@users.noreply.github.com>
2023-12-12 10:28:35 +0800

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,12 @@
+﻿using ClassLibrary1;
+using System.Runtime.CompilerServices;
 
+[assembly: IgnoresAccessChecksTo("ClassLibrary1")]
+
+var calculator = new Calculator();
+
+calculator.Value = "Hello World!";
+
+calculator.Calc();
+
+Console.ReadLine();
